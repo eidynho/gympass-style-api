b336baf2c302b5012c84a9b9dfb5308208af41b8
eidy <zviniciuseidy2@gmail.com>
2023-04-07 14:24:24 -0300

    new: in memory db for tests; new: should be able to register and should not be able to register with same email twice tests

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -1,5 +1,6 @@
+import { Prisma } from "@prisma/client";
+
 import { prisma } from "@/lib/prisma";
-import { Prisma, User } from "@prisma/client";
 
 import { UsersRepository } from "../users-repository";
 
