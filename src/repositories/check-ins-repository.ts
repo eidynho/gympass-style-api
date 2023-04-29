c032e32da7bed0541acae5387fc95f4a05a8f302
eidy <zviniciuseidy2@gmail.com>
2023-04-29 15:43:36 -0300

    new: prisma check-ins repository

--- src/repositories/check-ins-repository.ts
+++ src/repositories/check-ins-repository.ts
@@ -2,8 +2,8 @@ import { CheckIn, Prisma } from "@prisma/client";
 
 export interface CheckInsRepository {
     findById(id: string): Promise<CheckIn | null>;
-    findManyByUserId(userId: string, page: number): Promise<CheckIn[]>;
     findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>;
+    findManyByUserId(userId: string, page: number): Promise<CheckIn[]>;
     countByUserId(userId: string): Promise<number>;
     create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>;
     save(checkIn: CheckIn): Promise<CheckIn>;
