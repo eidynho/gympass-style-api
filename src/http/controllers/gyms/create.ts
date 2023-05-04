774b3addd694978e32c0545db62f1ddd16c1e18b
eidy <zviniciuseidy2@gmail.com>
2023-05-03 21:48:11 -0300

    new: gyms controllers (create, nearby and search) and routes

---
+++ src/http/controllers/gyms/create.ts
@@ -0,0 +1,35 @@
+import { FastifyRequest, FastifyReply } from "fastify";
+import { z } from "zod";
+
+import { makeCreateGymUseCase } from "@/use-cases/factories/make-create-gym-use-case";
+
+export async function create(request: FastifyRequest, reply: FastifyReply) {
+    const createGymBodySchema = z.object({
+        title: z.string(),
+        description: z.string().nullable(),
+        phone: z.string().nullable(),
+        latitude: z.number().refine((value) => {
+            return Math.abs(value) <= 90;
+        }),
+        longitude: z.number().refine((value) => {
+            return Math.abs(value) <= 180;
+        }),
+    });
+
+    const { title, description, phone, latitude, longitude } =
+        createGymBodySchema.parse(request.body);
+
+    const createGymUseCase = makeCreateGymUseCase();
+
+    // don't need try catch to handle error because don't have custom error
+    // so it will be handled globally
+    await createGymUseCase.execute({
+        title,
+        description,
+        phone,
+        latitude,
+        longitude,
+    });
+
+    return reply.status(201).send();
+}
